402cddfa0ad84b7b6524358d24f07cb65afe9107
AbrahamOrtegaM <70242757+AbrahamOrtegaM@users.noreply.github.com>
2020-10-27 19:19:52 -0700

    Update scritp.py

--- scritp.py
+++ scritp.py
@@ -1,23 +1,37 @@
-  
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
 
-# Ruta del archivo en formato GenBank del escritorio (Bacillum)
-filename = "/mnt/c/Users/abrah/Desktop/COMPUTACION/GCF_000321185.1_ASM32118v1_genomic.gbff"
+filename =  os.path.abspath("data/ls_orchid.gbk")
 
+# Definición de la función summarize_contents
 def summarize_contents(filename):
-listAOM = []
-listAOM = os.path.split(filename)
-print("file:", listAOM[1], "\npath:", listAOM[0])
-all_records = []
-records = list(SeqIO.parse(filename, "genbank"))
-print("num_records = %i records" % len(records))
-print("records:")
-for seq_record in SeqIO.parse(filename, "genbank"):
-all_records.append(seq_record.name)
-print("- id:",seq_record.id)
-print("name: ", seq_record.name)
-print("description: ", seq_record.description)
-print("\n")
-summarize_contents(filename)
+	listaRuta = os.path.split(filename)
+	extension = os.path.splitext(filename)
+	# Tipo de archivo genbank o fasta 
+	if(extension[1] == ".gbk"):
+		tfile = "genbank"
+	else: 
+		tfile = "fasta"
+	records = list(SeqIO.parse(filename, tfile))
+	# Crear diccionario 
+	diccionario = {}
+	# Nombre del archivo, ruta y número de registros
+	diccionario['file:'] = listaRuta[1]
+	diccionario['path:'] = listaRuta[0]
+	diccionario['num_records:'] = len(records)
+	# Crear listas en diccionario
+	diccionario['names:'] = []
+	diccionario['IDs:'] = []
+	diccionario['descriptions:'] = []
+	# Registros
+	for seq_record in SeqIO.parse(filename, tfile):
+		diccionario['names:'].append(seq_record.name)
+		diccionario['IDs:'].append(seq_record.id)
+		diccionario['descriptions:'].append(seq_record.description)
+	return diccionario
+
+# Llamada a la función summarize_contents
+if __name__ == "__main__":
+	resultado = summarize_contents(filename)
+	print(resultado)
